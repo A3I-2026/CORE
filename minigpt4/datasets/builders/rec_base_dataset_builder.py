58d85b09992c4dca449622d49266c8f71e90e928
AAAI---2026 <ws_zyz@163.com>
2026-07-31 17:50:23 +0800

    Update rec_base_dataset_builder.py

--- minigpt4/datasets/builders/rec_base_dataset_builder.py
+++ minigpt4/datasets/builders/rec_base_dataset_builder.py
@@ -1,10 +1,4 @@
-"""
- This file is from
- Copyright (c) 2022, salesforce.com, inc.
- All rights reserved.
- SPDX-License-Identifier: BSD-3-Clause
- For full license text, see the LICENSE_Lavis file in the repo root or https://opensource.org/licenses/BSD-3-Clause
-"""
+
 
 import logging
 import os
@@ -109,79 +103,6 @@ class RecBaseDatasetBuilder:
 
     def _download_data(self):
         pass
-        # self._download_ann()
-        # self._download_vis()
-
-    # def _download_ann(self):
-    #     """
-    #     Download annotation files if necessary.
-    #     All the vision-language datasets should have annotations of unified format.
-
-    #     storage_path can be:
-    #       (1) relative/absolute: will be prefixed with env.cache_root to make full path if relative.
-    #       (2) basename/dirname: will be suffixed with base name of URL if dirname is provided.
-
-    #     Local annotation paths should be relative.
-    #     """
-    #     anns = self.config.build_info.annotations
-
-    #     splits = anns.keys()
-
-    #     cache_root = registry.get_path("cache_root")
-
-    #     for split in splits:
-    #         info = anns[split]
-
-    #         urls, storage_paths = info.get("url", None), info.storage
-
-    #         if isinstance(urls, str):
-    #             urls = [urls]
-    #         if isinstance(storage_paths, str):
-    #             storage_paths = [storage_paths]
-
-    #         assert len(urls) == len(storage_paths)
-
-    #         for url_or_filename, storage_path in zip(urls, storage_paths):
-    #             # if storage_path is relative, make it full by prefixing with cache_root.
-    #             if not os.path.isabs(storage_path):
-    #                 storage_path = os.path.join(cache_root, storage_path)
-
-    #             dirname = os.path.dirname(storage_path)
-    #             if not os.path.exists(dirname):
-    #                 os.makedirs(dirname)
-
-    #             if os.path.isfile(url_or_filename):
-    #                 src, dst = url_or_filename, storage_path
-    #                 if not os.path.exists(dst):
-    #                     shutil.copyfile(src=src, dst=dst)
-    #                 else:
-    #                     logging.info("Using existing file {}.".format(dst))
-    #             else:
-    #                 if os.path.isdir(storage_path):
-    #                     # if only dirname is provided, suffix with basename of URL.
-    #                     raise ValueError(
-    #                         "Expecting storage_path to be a file path, got directory {}".format(
-    #                             storage_path
-    #                         )
-    #                     )
-    #                 else:
-    #                     filename = os.path.basename(storage_path)
-
-    #                 download_url(url=url_or_filename, root=dirname, filename=filename)
-
-    # def _download_vis(self):
-
-    #     storage_path = self.config.build_info.get(self.data_type).storage
-    #     storage_path = utils.get_cache_path(storage_path)
-
-    #     if not os.path.exists(storage_path):
-    #         warnings.warn(
-    #             f"""
-    #             The specified path {storage_path} for visual inputs does not exist.
-    #             Please provide a correct path to the visual inputs or
-    #             refer to datasets/download_scripts/README.md for downloading instructions.
-    #             """
-    #         )
 
     def build(self):
         """
